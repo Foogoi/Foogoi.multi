30e725d902dc14257dcb18c041f3062481828612
Phionx <shanjha.usa@gmail.com>
2015-05-26 14:18:23 -0400

    snake

--- snake.js
+++ snake.js
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
-$(document).ready(function () {
-=======
 $(document).ready(function ( ){
->>>>>>> Stashed changes
+
     var field = $("#field").getContext('2d');
     var width = $("#field").width();
     var height = $("#field").height();
