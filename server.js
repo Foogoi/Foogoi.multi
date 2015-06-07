13097dccb82016289f2c7b20d5de290e2c726cd4
Phionx <shanjha.usa@gmail.com>
2015-06-07 13:08:15 -0400

    build

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
- 
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    util = require('util'),
+    io = require('socket.io')(server);
+
+var socket,
+    players;
+
+//initializing an array of players
+function init() {
+    players = []
+}
+
+init();
+
+socket = io.listen(8000);
+
+socket.configure(function() {
+    socket.set("transports", ["websocket"]);
+    socket.set("log level", 2);
+});
+
+
+
+
+/*
 app.use(express.static(__dirname + '/public'));
  
+
 app.get('/', function(req, res){
   res.render('/index.html');
 });
  
-server.listen(8080);
+
 console.log("Multiplayer app listening on port 8080");
+*/
